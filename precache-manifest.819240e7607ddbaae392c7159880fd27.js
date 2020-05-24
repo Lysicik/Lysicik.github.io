7c5aa0ad8cbc0c0a722e84d65d46ffcb1eec2f63
Lysicik <Lysicik@mail.ru>
2020-05-25 00:51:45 +0300

    prod

---
+++ precache-manifest.819240e7607ddbaae392c7159880fd27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f25afcce7a6eb792f67a6407c5b27f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b498a760dd9a161c62",
+    "url": "/static/css/main.f2df5a96.chunk.css"
+  },
+  {
+    "revision": "7b872e61f02375a50a2d",
+    "url": "/static/js/2.1e2cd598.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/static/js/2.1e2cd598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b498a760dd9a161c62",
+    "url": "/static/js/main.0fe76dcf.chunk.js"
+  },
+  {
+    "revision": "24da96dbd1c571e781de",
+    "url": "/static/js/runtime-main.3c784964.js"
+  },
+  {
+    "revision": "8de89f191bac990b90f0be47881273b2",
+    "url": "/static/media/BG4.8de89f19.jpg"
+  }
+]);
